c3c4ca62c9898153d74c129d2c7b807689ed7aa7
Sthvnbb <sabolanosb@hotmail.com>
2025-04-10 17:30:27 -0600

    Adición de barra de búsqueda en los expedientes
    Barra de búsqueda que permite buscar en todos los campos de la tabla de expedientes

--- Hospital/Hospital/obj/Debug/net9.0/Hospital.AssemblyInfo.cs
+++ Hospital/Hospital/obj/Debug/net9.0/Hospital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384460dbff9d6c2421b477a29b1b3448da5bcd73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b80bf55d9781a0953b122a6d7cbfb32025dd23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
